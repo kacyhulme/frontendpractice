a0720fa4a14b69ed0bf906fbd5a58cd54b698bd7
kacy hulme <kacyhulme@gmail.com>
2020-05-09 18:31:21 -0400

    add filler values for db seed grift task

--- grifts/db.go
+++ grifts/db.go
@@ -10,12 +10,17 @@ var _ = grift.Namespace("db", func() {
 
 	grift.Desc("seed", "Seeds a database")
 	grift.Add("seed", func(c *grift.Context) error {
-		u := &models.User{
-			FirstName: "Axel",
-			LastName:  "Raine",
-			Email:     "axelraine@gmail.com",
-			City:      "Blue Hill",
-			State:     "ME",
+		u := &models.Users{
+			{FirstName: "Daddy",
+				LastName: "Pants",
+				Email:    "daddy@gmail.com",
+				City:     "Blue Hill",
+				State:    "ME"},
+			{FirstName: "Detroit",
+				LastName: "Pants",
+				Email:    "detroit@gmail.com",
+				City:     "Blue Hill",
+				State:    "ME"},
 		}
 		err := models.DB.Create(u)
 		return err
