ca5a73921f374eb5a704d558da30e88a2ef0fc87
kacy hulme <kacyhulme@gmail.com>
2020-07-15 08:02:00 -0400

    Add simple React to home page

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,8 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 require('jquery-ui-bundle');
+import React from "react";
+import ReactDOM from "react-dom";
 
 $(() => {
 
@@ -8,6 +10,27 @@ $(() => {
     $("#post-Date").datepicker();
   });
 
-  
-  
+  const e = React.createElement;
+
+  class LikeButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { liked: false };
+    }
+
+    render() {
+      if (this.state.liked) {
+        return 'You liked this.';
+      }
+
+      return e(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        'Like'
+      );
+    }
+  }
+  const domContainer = document.querySelector('#practice-container');
+  ReactDOM.render(e(LikeButton), domContainer);
+
 });
